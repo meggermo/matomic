eebf5983eff951a76ad74d4498282e8c963e868b
Michiel Eggermont <michiel.eggermont@gmail.com>
2012-09-08 11:28:13 +0200

    Changed string literals to keywords

--- test/matomic/test/core.clj
+++ test/matomic/test/core.clj
@@ -3,18 +3,14 @@
    [matomic.core]
    [midje.sweet]))
 
-(def foo "foo")
-(def bar "bar")
-(def baz "baz")
-(def zap "zap")
-(def m (zipmap (iterate inc 0) [foo bar baz]))
+(def m (zipmap (iterate inc 0) [:foo :bar :baz]))
 
 (tabular 
   (fact "assoc-when"
-    (assoc-when m ?keys (constantly zap)) => ?result) 
+    (assoc-when m ?keys (constantly :zap)) => ?result) 
     ?keys        ?result 
-    [0]          {0 zap 1 bar 2 baz} 
-    [1]          {0 foo 1 zap 2 baz} 
-    [2]          {0 foo 1 bar 2 zap} 
-    [0 1]        {0 zap 1 zap 2 baz})
+    [0]          {0 :zap 1 :bar 2 :baz} 
+    [1]          {0 :foo 1 :zap 2 :baz} 
+    [2]          {0 :foo 1 :bar 2 :zap} 
+    [0 1]        {0 :zap 1 :zap 2 :baz})
 
